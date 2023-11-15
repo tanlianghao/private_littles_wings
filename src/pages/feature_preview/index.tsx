ec37663e08c731755db5f7545a0b99fc0eaef15c
tanlianghao <ganzi128@163.com>
2023-11-15 17:37:12 +0800

    style: preivew样式完成

--- src/pages/feature_preview/index.tsx
+++ src/pages/feature_preview/index.tsx
@@ -1,100 +1,122 @@
 import React, { useEffect, useRef } from 'react';
+import {Swiper, SwiperSlide} from 'swiper/react';
+import { Navigation, Pagination } from 'swiper/modules';
+// import Swiper and modules styles
+import 'swiper/scss';
+import 'swiper/scss/navigation';
+import 'swiper/scss/pagination';
 
 import './index.scss';
 
 const FeaturePreview = () => {
-    let imageList = [
-        require('@assets/feature_view/background_1.png'),
-        require('@assets/feature_view/background_2.png'),
-        require('@assets/feature_view/background_3.png'),
-        require('@assets/feature_view/background_4.png'),
-        require('@assets/feature_view/background_5.png'),
-        require('@assets/feature_view/background_6.png'),
-        require('@assets/feature_view/background_7.png'),
-    ];
+	let imageList = [
+			require('@assets/feature_view/background_1.png'),
+			require('@assets/feature_view/background_2.png'),
+			require('@assets/feature_view/background_3.png'),
+			require('@assets/feature_view/background_4.png'),
+			require('@assets/feature_view/background_5.png'),
+			require('@assets/feature_view/background_6.png'),
+			require('@assets/feature_view/background_7.png'),
+	];
 
-    const elementRef = useRef(null);
+	let swiperContainer = new Array(3).fill(imageList);
 
-    useEffect(() => {
-        new WaterFallView().layout(elementRef);
-    });
+	const elementRef = useRef(null);
 
-    return (
-        <>
-            <div className='mainBody'>
-                <div className='swiperBody'>
-                    <div className='waterfallsFlow' ref={elementRef}>
-                        {imageList.map((item, index) => {
-                            return (
-                                <img src={item} alt={'image_' + index} key={index}/>
-                            );
-                        })}
-                    </div>
-                    <h1>Find your favourite events here</h1>
-                    <div className='content-main'>
-                        <p>The customer is very important, the</p>
-                        <p>customer will be followed by the customer.</p>
-                    </div>
-                </div>
-                <button className='nextButton'>Next</button>
-            </div>
-        </>
-    );
+	useEffect(() => {
+			new WaterFallView().layout(elementRef);
+	});
+
+	return (
+		<>
+			<div className='mainBody'>
+				<Swiper
+					modules={[Navigation, Pagination]}
+					pagination={{ el: '.swiper-pagination', 
+						bulletClass: 'swiper-pagination-customer-bullet',
+						bulletActiveClass: 'swiper-pagination-customer-bullet-active',
+					}}
+				>
+						{
+							swiperContainer.map((images, index) => {
+								return (<SwiperSlide key={'slide_' + index} className='swiperBody'>
+									<div className='waterfallsFlow'>
+													{images.map((item: string, index: React.Key) => {
+															return (
+																	<img src={item} alt={'image_' + index} key={index}/>
+															);
+													})}
+											</div>
+											<h1>Find your favourite events here</h1>
+											<div className='content-main'>
+												<p>The customer is very important, the</p>
+												<p>customer will be followed by the customer.</p>
+											</div>
+								</SwiperSlide>);
+							})
+						}
+						<div className="swiper-pagination"></div>
+				</Swiper>
+				<button className='nextButton'>Next</button>
+			</div>
+		</>
+	);
 }
 
 export default FeaturePreview;
 
 class WaterFallView {
-    cacheHeightByItem = [];
 
-    layout(ref: React.MutableRefObject<any>) {
-        let childrens = ref.current.children;
-        let containerWidth = ref.current.offsetWidth;
-        let containerHeight = ref.current.offsetHeight;
-        const columnNo = 3;
-        const space = 5.0;
-        let top = 0, left = 0;
+	layout(ref: React.MutableRefObject<any>) {
+		let imageWaterFallsDom = document.getElementsByClassName('waterfallsFlow');
+		const columnNo = 3;
+		const space = 5.0;
+		
+		for (let i = 0; i< imageWaterFallsDom.length; i++) {
+			let containerElement = imageWaterFallsDom[i] as HTMLElement;
+			let containerWidth = containerElement.offsetWidth;
+			let containerHeight = containerElement.offsetHeight;
+			let imageWidth = (containerWidth / columnNo) - space;
+			let childrens = imageWaterFallsDom[i].children as HTMLCollectionOf<HTMLImageElement>;	
+			let top = 0, left = 0;
+			let cacheHeightByItem = [];
+			childrens[0].onload = () => {
+				for (let i = 0; i < childrens.length; i++) {
+					let itemDom = childrens[i] as HTMLImageElement;
 
-        let imageWidth = (containerWidth / columnNo) - space;
+					if (i < columnNo) {
+						left = (imageWidth + space) * i;
+						cacheHeightByItem.push(itemDom.offsetHeight);
+					} else {
+						const index = this.getMinHeightIndex(cacheHeightByItem);
+						left = childrens[index].offsetLeft;
+						top = childrens[index].offsetHeight + space;
+						cacheHeightByItem[index] += top;
+						
+						let currentItemPostion = top + itemDom.offsetHeight;
 
-        childrens[0].onload = () => {
-            for (let i = 0; i < childrens.length; i++) {
-                let itemDom = childrens[i] as HTMLElement;
+						if (currentItemPostion < containerHeight) {
+							let itemHeight = containerHeight - top;
+							itemDom.style.height = itemHeight + 'px';
+						}
+					}
+					itemDom.style.top = top + 'px';
+					itemDom.style.left = left + 'px';
+				}
+			}
+		}
+	}
 
-                if (i < columnNo) {
-                    left = (imageWidth + space) * i;
-                    this.cacheHeightByItem.push(itemDom.offsetHeight);
-                } else {
-                    const index = this.getMinHeightIndex();
-                    left = childrens[index].offsetLeft;
-                    top = childrens[index].offsetHeight + space;
-                    this.cacheHeightByItem[index] += top;
-                    
-                    let currentItemPostion = top + itemDom.offsetHeight;
-    
-                    if (currentItemPostion < containerHeight) {
-                        let itemHeight = containerHeight - top;
-                        itemDom.style.height = itemHeight + 'px';
-                    }
-                }
-                itemDom.style.top = top + 'px';
-                itemDom.style.left = left + 'px';
-            }
-        }
+	getMinHeightIndex(cacheHeightByItem: number[]) {
+		let minIndex = 0;
+		let currentMinValue = cacheHeightByItem[minIndex];
 
-    }
-
-    getMinHeightIndex() {
-        let minIndex = 0;
-        let currentMinValue = this.cacheHeightByItem[minIndex];
-
-
-        for (let i = 0; i < this.cacheHeightByItem.length; i++) {
-            if (currentMinValue > this.cacheHeightByItem[i]) {
-                minIndex = i;
-                currentMinValue = this.cacheHeightByItem[i];
-            }
-        }
-        return minIndex;
-    }
+		for (let i = 0; i < cacheHeightByItem.length; i++) {
+			if (currentMinValue > cacheHeightByItem[i]) {
+				minIndex = i;
+				currentMinValue = cacheHeightByItem[i];
+			}
+		}
+		return minIndex;
+	}
 }
